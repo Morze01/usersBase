c1068e8653a1599559805c98a92d43da5754dc53
morze <amorozov@list.ru>
2019-03-18 12:40:29 +0300

    update Authorise filter 2

--- src/main/java/filter/AuthoriseFilter.java
+++ src/main/java/filter/AuthoriseFilter.java
@@ -9,37 +9,28 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/*"})
+@WebFilter(urlPatterns = {"","/*"})
 public class AuthoriseFilter implements Filter {
     private String encoding;
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        encoding = filterConfig.getInitParameter("requestEncoding");
-        if (encoding == null) encoding = "UTF-8";
-    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
 
-        if (null == request.getCharacterEncoding()) {
-            request.setCharacterEncoding(encoding);
-        }
-
         User user = (User) ((HttpServletRequest) request).getSession().getAttribute("user");
         String currentUrl = ((HttpServletRequest) request).getRequestURI();
 
-        if (user == null || "/".equals(currentUrl)) {
-            RequestDispatcher dispatcher = request.getRequestDispatcher("/login.jsp");
-            dispatcher.forward(request, response);
+        boolean userCheck = user != null;
+        boolean urlCheck = "/login".equals(currentUrl);
+
+        if ((userCheck || urlCheck) && !"/".equals(currentUrl)) {
+            filterChain.doFilter(request, response);
+        } else {
+            ((HttpServletResponse) response).sendRedirect("/login");
         }
 
-        filterChain.doFilter(request, response);
+
 
     }
 
-    @Override
-    public void destroy() {
-
-    }
 }
